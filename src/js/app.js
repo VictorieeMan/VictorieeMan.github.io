08605fc58dd958cffd3c3fa4dc721d044b83d96e
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2022-06-26 17:42:53 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   new SweetScroll({/* some options */});
 
-  particlesJS.load(@dom-id, @path-json, @callback (optional));
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
   particlesJS('particles-js', {
   "particles": {
     "number": {
